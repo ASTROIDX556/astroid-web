8b092354f58c1a6d723b14eb47e11e5b5c76c348
Ghostcode <all4godempire@gmail.com>
2026-08-31 11:38:51 +0100

    feat: feat: implement dynamic spend limit rules and policy builder (#34)

--- src/features/policies/index.ts
+++ src/features/policies/index.ts
@@ -1,3 +1,5 @@
 export * from './BudgetSimulator';
 export * from './types';
 export { default as BudgetSimulator } from './BudgetSimulator';
+export { default as PolicyForm } from './components/PolicyForm';
+export * from './schemas/policySchema';
