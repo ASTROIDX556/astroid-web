ca2550e0ac2a8f5d3d3a8c1f3dc2c1085a388aa6
Ghostcode <all4godempire@gmail.com>
2026-08-31 12:25:34 +0100

    fix(ci): resolve failing checks for #34

--- src/features/policies/index.ts
+++ src/features/policies/index.ts
@@ -1,5 +1,5 @@
 export * from './BudgetSimulator';
-epport * from './types';
+etport * from './types';
 export { default as BudgetSimulator } from './BudgetSimulator';
 export { default as PolicyForm } from './components/PolicyForm';
 export * from './schemas/policySchema';
