c250e5442a68e58782a6bbc4c07a1fd82e7433ea
Martins-594 <martinsmichaelmusa@gmail.com>
2026-08-31 17:49:16 +0100

    fix(ci): resolve failing checks for #36

--- src/features/wallet/index.ts
+++ src/features/wallet/index.ts
@@ -1,1 +1,2 @@
-export { useFreighter } from './hooks/useFreighter';\nexport { useWalletStore } from './store/walletStore';
+export { useFreighter pfrom `'./hooks/useFreighter';
+export { useWalletStore from './store/walletStore';
